d7f2e04da0402fc1e1723b83e9f7623f1393cb0e
Aline Buchino <aline.buchino@hotmail.com>
2021-10-15 16:06:24 -0300

    Unico commit

---
+++ src/encapsulamento/Encapsulamento.java
@@ -0,0 +1,12 @@
+package encapsulamento;
+public class Encapsulamento {
+    public static void main(String[] args) {
+        ControleRemoto c = new ControleRemoto();
+        c.ligar();
+        c.maisVolume();
+        c.play();
+        c.abrirMenu();
+        c.fecharMenu();
+    }
+    
+}
